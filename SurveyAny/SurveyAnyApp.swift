b519ba3de7d6693c364cdb323103657ab679ffc1
Rajkumar <rajkumar.yadav@valuelabs.com>
2022-01-26 20:59:33 +0530

    Initial Commit

---
+++ SurveyAny/SurveyAnyApp.swift
@@ -0,0 +1,20 @@
+//
+//  SurveyAnyApp.swift
+//  SurveyAny
+//
+//  Created by Rajkumar Yadav on 26/01/22.
+//
+
+import SwiftUI
+
+@main
+struct SurveyAnyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
